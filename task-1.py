0f2cef6ae57dccc5945ff0aa7a5545866a7f6c4a
iHnKwuuN <6nMnueBijq>
2022-02-20 12:57:24 +0400

    week-1

---
+++ task-1.py
@@ -0,0 +1,51 @@
+import pandas
+import re
+
+data = pandas.read_csv('titanic.csv', index_col='PassengerId')
+
+passenger_count = len(data.index)
+
+# Какое количество мужчин и женщин ехало на корабле?
+print(data['Sex'].value_counts())
+
+# Какой части пассажиров удалось выжить? Посчитайте долю выживших пассажиров.
+survived_count = data[data["Survived"] == 1].count().values[1]
+print(round(survived_count / passenger_count * 100), 2)
+
+# Какую долю пассажиры первого класса составляли среди всех пассажиров? Ответ приведите в процентах.
+first_class = data[data["Pclass"] == 1].count().values[1]
+print(round(first_class / passenger_count * 100), 2)
+
+# Какого возраста были пассажиры? Посчитайте среднее и медиану возраста пассажиров.
+print(data['Age'].mean())
+print(data['Age'].median())
+
+# Коррелируют ли число братьев/сестер с числом родителей/детей?
+# Посчитайте корреляцию Пирсона между признаками SibSp и Parch.
+print(round(data['SibSp'].corr(data['Parch'])), 2)
+
+# Какое самое популярное женское имя на корабле?
+data_female = data[data["Sex"] == 'female']
+
+
+def first_name(name):
+    # Первое слово до запятой - фамилия
+    s = re.search('^[^,]+, (.*)', name)
+    if s:
+        name = s.group(1)
+
+    # Если есть скобки - то имя пассажира в них
+    s = re.search('\(([^)]+)\)', name)
+    if s:
+        name = s.group(1)
+    # Удаляем обращения
+    name = re.sub('(Miss\. |Mrs\. |Ms\. )', '', name)
+    # Берем первое оставшееся слово и удаляем кавычки
+    name = name.split(' ')[0].replace('"', '')
+    return name
+
+
+names = data[data['Sex'] == 'female']['Name'].map(first_name)
+name_counts = names.value_counts()
+
+print(name_counts.head(1).index.values[0])
---
+++ task-2.py
@@ -0,0 +1,32 @@
+import pandas
+from sklearn.tree import DecisionTreeClassifier
+
+# Загрузите выборку из файла titanic.csv с помощью пакета Pandas.
+data_raw = pandas.read_csv('titanic.csv')
+
+# Оставьте в выборке четыре признака: класс пассажира (Pclass), цену билета (Fare),
+# возраст пассажира (Age) и его пол (Sex).
+data = data_raw.drop(columns=['PassengerId', 'Name', 'SibSp', 'Parch', 'Ticket', 'Cabin', 'Embarked'])
+
+# В данных есть пропущенные значения — например, для некоторых
+# пассажиров неизвестен их возраст. Такие записи при чтении их в
+# pandas принимают значение nan. Найдите все объекты, у которых
+# есть пропущенные признаки, и удалите их из выборки.
+data.dropna(inplace=True)
+
+# Обратите внимание, что признак Sex имеет строковые значения
+data = data.replace({'Sex': {'female': 1, 'male': 0}})
+
+# Выделите целевую переменную — она записана в столбце Survived.
+survived = data['Survived']
+data = data.drop(columns='Survived')
+
+# Обучите решающее дерево с параметром random_state=241 и остальными параметрами по умолчанию.
+clf = DecisionTreeClassifier(random_state=421)
+clf.fit(data, survived)
+
+# Вычислите важности признаков и найдите два признака с наибольшей важностью.
+importances = clf.feature_importances_
+
+print(data.head())
+print(importances)
